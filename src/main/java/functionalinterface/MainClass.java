ff87eb336a3d39fa416a6658e7a09033e8bbc93f
mshemeel007 <mshemeel007@gmail.com>
2023-05-18 19:20:50 +0530

    NEW commit

--- src/main/java/functionalinterface/MainClass.java
+++ src/main/java/functionalinterface/MainClass.java
@@ -1,7 +1,12 @@
 package main.java.functionalinterface;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
@@ -14,8 +19,31 @@ public class MainClass {
         SampleFunctionalInterface fi = () -> "Hello";
         System.out.println(fi.hello());
 
+        // calling default method
+        fi.saySome();
+        fi.saySome2();
+
         //Functional Interface with arguments
         MyFunctionalInterface myFI = x ->  "Hello "+x;
         System.out.println(myFI.printName("Shemeel"));
+
+        // Pre-defined functional interfaces
+        //Predicate -> consumes the arg and return boolean
+        Predicate<Integer> predicate = x -> x > 5;
+        System.out.println(predicate.test(10));
+
+        //Supplier -> does not accept any arg but returns an arg
+        Supplier<String> supplier = () -> "Hello World";
+        System.out.println(supplier.get());
+
+        //Consumer -> consumes the arg but returns no value
+        Consumer<String> consumer = x -> System.out.println("Hello "+x);
+        consumer.accept("Shemeel");
+
+        //Function -> accepts and arg and returns a value
+        Function<Integer, String> function = x -> "The number is :" + x;
+        System.out.println(function.apply(10));
+
+
     }
 }
--- src/main/java/functionalinterface/SampleFunctionalInterface.java
+++ src/main/java/functionalinterface/SampleFunctionalInterface.java
@@ -3,11 +3,12 @@ package main.java.functionalinterface;
 public interface SampleFunctionalInterface {
     public String hello();
 
+    // we can have multiple default methods in a functional interface
     default public void saySome(){
-        System.out.println("JHello");
+        System.out.println("JHello SaySome");
     }
 
     default public void saySome2(){
-        System.out.println("JHello");
+        System.out.println("JHello SaySome2");
     }
 }
